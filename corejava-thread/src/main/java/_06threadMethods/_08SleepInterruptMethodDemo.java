fbb7ae902d909c102e5740effc4914b800927e85
Taher <Taher@192.168.1.7>
2019-04-13 20:22:28 +0530

    Thread

--- corejava-thread/src/main/java/_06threadMethods/_08SleepInterruptMethodDemo.java
+++ corejava-thread/src/main/java/_06threadMethods/_08SleepInterruptMethodDemo.java
@@ -1,17 +1,28 @@
 package _06threadMethods;
 
+/*
+A thread can interrupt another thread by calling interrupt() of Thread class.
+If the target thread is not in sleeping state or waiting state, then there is
+no impact of interrupt call immediately. Interrupt call be waited until target
+thread entered into sleeping or waiting state.
+If target thread entered into sleeping or waiting state, then immediately 
+interrupt call will interrupt target thread.
+If target thread never entered into sleeping or waiting state then there is no impact
+of interrupt call.
+
+In below program child thread will enter into sleeping state then interrupt call at line 23
+will interrupt child thread. 
+*/
+
 class MyThreadO extends Thread {
 
 	public void run() {
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Lazy Thread");
-		}
 		try {
 			for (int i = 0; i < 5; i++) {
-				System.out.println("Sleeping");
+				System.out.println("Lazy Thread");
 				Thread.sleep(1000);
 			}
-		} catch (Exception e) {
+		} catch (InterruptedException e) {
 			System.out.println("exception because " + e.getMessage());
 		}
 	}
--- corejava-thread/src/main/java/_06threadMethods/_09SleepInterruptMethodDemo.java
+++ corejava-thread/src/main/java/_06threadMethods/_09SleepInterruptMethodDemo.java
@@ -1,15 +1,24 @@
 package _06threadMethods;
 
+/*
+A thread can interrupt another thread by calling interrupt() of Thread class.
+If the target thread is not in sleeping state or waiting state, then there is
+no impact of interrupt call immediately. Interrupt call be waited until target
+thread entered into sleeping or waiting state.
+If target thread entered into sleeping or waiting state, then immediately 
+interrupt call will interrupt target thread.
+If target thread never entered into sleeping or waiting state then there is no impact
+of interrupt call.
+
+In below program child thread won't enter into sleeping state then interrupt call at line 31
+will never interrupt child thread. 
+*/
+
 class MyThreadN extends Thread {
 
 	public void run() {
-		try {
-			for (int i = 0; i < 5; i++) {
-				System.out.println("Lazy Thread");
-				Thread.sleep(1000);
-			}
-		} catch (InterruptedException e) {
-			System.out.println("exception because " + e.getMessage());
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Lazy Thread");
 		}
 	}
 }
