3ddb1b4e09e414a1fe5f8e5c67a140a2ce610ae8
Taher <Taher@192.168.1.6>
2019-05-12 09:31:38 +0530

    ListIterator

--- corejava-collection/src/main/java/_08listIteratorInterface/_01ListIteratorMethodDemo.java
+++ corejava-collection/src/main/java/_08listIteratorInterface/_01ListIteratorMethodDemo.java
@@ -1,4 +1,16 @@
 package _08listIteratorInterface;
+/*
+ * Iterator will be used to iterate through a set/list/queue. All these collections have their own logic for ordering. 
+ * For e.g "TreeSet" is a sorted data structure. Now while iterating through a treeSet of let {2,5,7,19,22}; when the cursor is at 7 ,programmer tries to add 29. 
+ * in this scenario treeset would be dis-sorted and corrupted.Hence add method should not be allowed.
+
+ * But in remove () we are only deleting an element from a particular
+ * position which can never harm the ordering. So its allowed.
+
+ * Coming to ListIterator which allows add(). Remember its used only for List, which maintains order of its elements according to insertion
+ * order.So if we add an element at any given point , the ordering
+ * algorithm wont be hampered.
+ */
 
 public class _01ListIteratorMethodDemo {
 
