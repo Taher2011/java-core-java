97e67c6c17a88a9871b2c529f8895911ef1ad56e
Taher <Taher@192.168.1.2>
2019-06-06 13:58:21 +0530

    constructor

--- corejava-oops/src/main/java/_13constructor/_04ConstructorDemo.java
+++ corejava-oops/src/main/java/_13constructor/_04ConstructorDemo.java
@@ -15,8 +15,15 @@ class F extends E {
 public class _04ConstructorDemo {
 
 	public static void main(String[] args) {
+		
 		E e = new E();
+		System.out.println();
+		
 		F f = new F();
+		System.out.println();
+		
+		E ef = new F();
+		System.out.println();
 		
 		System.out.println(f.hashCode());
 	}
