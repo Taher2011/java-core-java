418678d6150a07403d9d5ee5a78c55fbf12e8c5d
tali <tali@IN-tali-W2.adprod.bmc.com>
2020-09-26 10:04:16 +0530

    Method reference

---
+++ corejava-java8/src/main/java/com/java/_10methodReference/_00Introduction.java
@@ -0,0 +1,15 @@
+package com.java._10methodReference;
+
+// Functional interface method can be mapped to our specified method by using :: operator. This is called method reference.
+
+// Our specified method can be either static method or instance method.
+
+// Functional interface method and our specified method should have same argument types, except this the remaining things like
+// return type, method name, modifiers are not required to match.
+
+// Functional interface can refer lambda expressions and can also refer method reference. Hence method reference is alternative to 
+// lambda expressions.
+
+public class _00Introduction {
+
+}
