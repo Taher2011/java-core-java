95b9038290828b7c8a7bd63c9162a7610fb678cd
ADPROD\tali <taher_ali_tp@bmc.com>
2021-01-30 18:01:44 +0530

    refactored

--- corejava-garbagecollection/src/main/java/_03finalization/_01FinalizeDemo.java
+++ corejava-garbagecollection/src/main/java/_03finalization/_01FinalizeDemo.java
@@ -3,6 +3,7 @@ package _03finalization;
 class Employee {
 
 	public void finalize() throws Throwable {
+		System.out.println("object " + this.hashCode());
 		System.out.println("Finalize method called on employee object");
 	}
 }
@@ -11,9 +12,11 @@ public class _01FinalizeDemo {
 
 	public static void main(String[] args) throws InterruptedException {
 		Employee employee = new Employee();
+		System.out.println("object " + employee.hashCode());
 		employee = null;
 		System.out.println("Calling GC");
-		System.gc();// When GC call finalize(), implicitly finalize() method will be called only once on that object and object would be destroyed
+		System.gc();// When GC call finalize(), implicitly finalize() method will be called only
+					// once on that object and object would be destroyed
 		Thread.sleep(100);
 		System.out.println("End of main method");
 	}
