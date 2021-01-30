ffee69c9503f4b48ff83957ff4dd81196bb3708f
ADPROD\tali <taher_ali_tp@bmc.com>
2021-01-30 18:17:49 +0530

    refactored

--- corejava-garbagecollection/src/main/java/_03finalization/_03GCCallFinalizeOnlyOnceForAnyObjectDemo.java
+++ corejava-garbagecollection/src/main/java/_03finalization/_03GCCallFinalizeOnlyOnceForAnyObjectDemo.java
@@ -1,26 +1,40 @@
 package _03finalization;
 
+class Animal {
+
+	public void finalize() {
+		System.out.println(this.hashCode());
+		_03GCCallFinalizeOnlyOnceForAnyObjectDemo.globalAnimal = this;
+		System.out.println("finalize() called");
+	}
+
+}
+
 public class _03GCCallFinalizeOnlyOnceForAnyObjectDemo {
 
-	static _03GCCallFinalizeOnlyOnceForAnyObjectDemo outerDemo;
-	
+	static Animal globalAnimal;
+
 	public static void main(String[] args) throws InterruptedException {
-		_03GCCallFinalizeOnlyOnceForAnyObjectDemo innerDemo = new _03GCCallFinalizeOnlyOnceForAnyObjectDemo();
-		System.out.println("hash code for object is " + innerDemo.hashCode());
-		innerDemo = null;
+
+		Animal localAnimal = new Animal();
+		System.out.println("hash code for object is " + localAnimal.hashCode());
+		
+		localAnimal = null;
 		System.out.println("Calling GC");
 		System.gc();
+		
 		Thread.sleep(5000);
-		System.out.println("hash code for object is " + outerDemo.hashCode());
-		outerDemo = null;
+		
+		System.out.println("hash code for object is " + globalAnimal.hashCode());
+		
+		globalAnimal = null;
 		System.out.println("Calling GC again on same object");
 		System.gc();
+		
 		Thread.sleep(5000);
+		
 		System.out.println("End of main method");
+
 	}
 
-	public void finalize() throws Throwable {
-		System.out.println("Finalize method called");
-		outerDemo = this;
-	}
 }
