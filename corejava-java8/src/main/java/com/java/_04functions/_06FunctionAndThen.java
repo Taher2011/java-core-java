7db488e152aa5a03b7352d513c226d39a5e14aaa
ADPROD\tali <taher_ali_tp@bmc.com>
2021-02-15 17:37:22 +0530

    refactored

--- corejava-java8/src/main/java/com/java/_04functions/_06FunctionAndThen.java
+++ corejava-java8/src/main/java/com/java/_04functions/_06FunctionAndThen.java
@@ -5,11 +5,14 @@ import java.util.function.Function;
 public class _06FunctionAndThen {
 
 	public static void main(String[] args) {
-
+		/*
+		 * f1.andThen(f2).apply(2); => first execute f1.apply(10) method and based on
+		 * output first method, f2.apply(result_of_f1_method) method execute.
+		 */
 		Function<Integer, Integer> f1 = i -> i * 2;
 		Function<Integer, Integer> f2 = i -> i * i * i;
 		
-		System.out.println(f1.andThen(f2).apply(20));
+		System.out.println(f1.andThen(f2).apply(2));
 
 	}
 
--- corejava-java8/src/main/java/com/java/_04functions/_07FunctionCompose.java
+++ corejava-java8/src/main/java/com/java/_04functions/_07FunctionCompose.java
@@ -6,9 +6,13 @@ public class _07FunctionCompose {
 
 	public static void main(String[] args) {
 
+		/*
+		 * f1.compose(f2).apply(2); => here just opposite to andThen() method. first
+		 * f2.apply() and then f1.apply(output_of_f2_method) execute.
+		 */
 		Function<Integer, Integer> f1 = i -> i * 2;
 		Function<Integer, Integer> f2 = i -> i * i * i;
-		
+
 		System.out.println(f1.compose(f2).apply(2));
 
 	}
