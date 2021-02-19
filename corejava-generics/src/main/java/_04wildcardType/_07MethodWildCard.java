4dc85209718e69c14acbb2a1cc3e2d1a7a4f64a9
ADPROD\tali <taher_ali_tp@bmc.com>
2021-02-19 17:48:01 +0530

    refactored

--- corejava-generics/src/main/java/_04wildcardType/_07MethodWildCard.java
+++ corejava-generics/src/main/java/_04wildcardType/_07MethodWildCard.java
@@ -6,7 +6,19 @@ import java.util.List;
 public class _07MethodWildCard {
 
 	public static <E> void print(List<E> list) {
-	 	list.stream().forEach(System.out::println);
+		list.stream().forEach(System.out::println);
+	}
+
+	public static void printL(List<?> list) { // same as above method at line 8
+		list.stream().forEach(System.out::println);
+	}
+
+	public static <E extends Number> void printNumber(List<E> list) {
+		list.stream().forEach(System.out::println);
+	}
+
+	public static void printNumber1(List<? extends Number> list) { // same as above method at line 16
+		list.stream().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
@@ -14,7 +26,7 @@ public class _07MethodWildCard {
 		list.add("A");
 		list.add("B");
 		print(list);
-		
+
 		List<Integer> listInt = new ArrayList<>();
 		listInt.add(1);
 		listInt.add(2);
