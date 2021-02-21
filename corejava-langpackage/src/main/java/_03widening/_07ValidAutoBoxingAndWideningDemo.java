d71b64f6fe8523123b16237f6bbe3e9bfa0db1b6
ADPROD\tali <taher_ali_tp@bmc.com>
2021-02-21 06:59:13 +0530

    refactored

--- corejava-langpackage/src/main/java/_03widening/_07ValidAutoBoxingAndWideningDemo.java
+++ corejava-langpackage/src/main/java/_03widening/_07ValidAutoBoxingAndWideningDemo.java
@@ -9,6 +9,7 @@ public class _07ValidAutoBoxingAndWideningDemo {
 		Long L1 = 10L;   //AutoBoxing
 	//	Long L2 = 10;
 		long l3 = 10;    //Widening
+		long l4 = 10L;    //AutoUnBoxing
 		Object O = 10;   //AutoBoxing followed by Widening
 		double d1 = 10;
 	//	Double D1 = 10;
