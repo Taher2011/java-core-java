8f6cefcc289792a1d057729856e9a401b01e948b
ADPROD\tali <taher_ali_tp@bmc.com>
2021-02-23 18:56:11 +0530

    refactored

--- corejava-innerclass/src/main/java/_01innerClasses/_06AccessingInnerClassFromOutsideOfOuterClass.java
+++ corejava-innerclass/src/main/java/_01innerClasses/_06AccessingInnerClassFromOutsideOfOuterClass.java
@@ -1,5 +1,7 @@
 package _01innerClasses;
 
+import _01innerClasses.Outer6.Inner6;
+
 class Outer6 {
 
 	class Inner6 {
@@ -10,6 +12,13 @@ class Outer6 {
 }
 
 public class _06AccessingInnerClassFromOutsideOfOuterClass {
+
+	public void m2() {
+		Outer6 outer = new Outer6();
+		Outer6.Inner6 inner = outer.new Inner6();
+		inner.m1();
+
+	}
 
 	public static void main(String[] args) {
 		Outer6 outer = new Outer6();
